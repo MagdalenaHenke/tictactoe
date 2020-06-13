18cc983efaf71f4f3043f783528a13028a85438d
lena <lena@floored.com>
2020-06-12 22:24:22 -0400

    diplay fields in three different rows

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,15 +4,28 @@ import Field from './Field.jsx';
 // this component will display all the fields
 // LEENA: obviously, make this better
 function Board({ board, playField }) {
+  const getField = (i) => {
+    return <Field key={i} value={board[i]} onClick={() => playField(i)} />;
+  };
+
+  // LEENA: maybe automate rendering rows
   return (
     <div>
-      {board.map((fieldValue, index) => (
-        <Field
-          key={index}
-          value={fieldValue}
-          onClick={() => playField(index)}
-        />
-      ))}
+      <div>
+        {getField(0)}
+        {getField(1)}
+        {getField(2)}
+      </div>
+      <div>
+        {getField(3)}
+        {getField(4)}
+        {getField(5)}
+      </div>
+      <div>
+        {getField(6)}
+        {getField(7)}
+        {getField(8)}
+      </div>
     </div>
   );
 }
