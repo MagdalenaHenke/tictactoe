3c31c82c32183d7d2a35c3c402a7539fd9771d11
lena <lena@floored.com>
2020-06-16 15:59:15 -0400

    whoCanWinTest

--- src/utils/autoPlayerUtil.js
+++ src/utils/autoPlayerUtil.js
@@ -2,7 +2,7 @@ import * as brd from '../utils/boardUtil';
 
 const DIFFICULTY = {
   // export these?
-  DEFAULT: 'easy',
+  DEFAULT: 'hard',
   SUPER_EASY: 'easyPeasy',
   EASY: 'easy',
   HARD: 'hard'
@@ -65,8 +65,83 @@ const playSmartField = (board) => {
   return playRandomField(board);
 };
 
+// a cats game is not a win for the other person
+// I'm just trying to avoid definite losses
+// maybe improve this to favor plays that make you win
+const memo = {};
+
+const whoCanWin = (board) => {
+  if (memo[board] !== undefined) return memo[board];
+  const canWin = { X: false, O: false };
+  if (brd.isCatsGame(board)) return canWin;
+  const winner = brd.getWinner(board);
+  if (winner) {
+    canWin[winner] = true;
+    memo[board] = canWin;
+    console.log('whoCanWin', board, canWin);
+    return canWin;
+  }
+
+  // if there is a smart field, you gotta play that one
+  const smartField = determineSmartField(board);
+  if (smartField !== null) {
+    const testBoard = brd.playField(board, smartField);
+    if (brd.isCatsGame(testBoard)) return canWin;
+    const winner = brd.getWinner(testBoard);
+    if (winner) {
+      canWin[winner] = true;
+      memo[board] = canWin;
+      console.log('whoCanWin', board, canWin);
+      return canWin;
+    }
+  }
+
+  let whoDefinitelyLosesIfIPlay = {};
+  const nextPlayer = brd.nextPlayer(board);
+  for (let i = 0; i < board.length; i++) {
+    // only consider empty boards
+    if (!board[i]) {
+      const testBoard = board.slice();
+      testBoard[i] = nextPlayer;
+      whoDefinitelyLosesIfIPlay[i] = whoCanWin(testBoard);
+    }
+  }
+
+  let xLosesAll = true;
+  let oLosesAll = true;
+  let possibleWinners = Object.values(whoDefinitelyLosesIfIPlay);
+  for (let i = 0; i < possibleWinners.length; i++) {
+    const winner = possibleWinners[i];
+    if (winner === 'X') xLosesAll = false;
+    if (winner === 'O') oLosesAll = false;
+  }
+
+  canWin.X = !xLosesAll;
+  canWin.O = !oLosesAll;
+  memo[board] = canWin;
+  console.log('whoCanWin', board, canWin);
+  return canWin;
+};
+
 // Think about algorithm for this
+// I don't wanna make any move where the other person _can_ win
 const playBestField = (board) => {
+  // if there is a smart field, you gotta play that one
+  const smartField = determineSmartField(board);
+  if (smartField !== null) return brd.playField(board, smartField);
+
+  const nextPlayer = brd.nextPlayer(board);
+  for (let i = 0; i < board.length; i++) {
+    const testBoard = board.slice();
+    if (!testBoard[i]) {
+      // only consider empty boards
+      testBoard[i] = nextPlayer;
+      // if this prevents the other person from winning, make that move
+      if (!whoCanWin(testBoard)[getOpponent(nextPlayer)]) return testBoard;
+    }
+  }
+
+  console.log('we got here, somehow');
   return board;
 };
 
