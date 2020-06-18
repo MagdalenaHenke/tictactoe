b5d1b944aeffd7b31c800b26e76df343c05a6271
lena <lena@floored.com>
2020-06-17 23:29:56 -0400

    moving create-react-app files into current folder structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../components/App.jsx';
+
+// LEENA: use this or get rid of it
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './styles/_cssReset.css';
-import './styles/_cssUtil.css';
-import './styles/App.css';
-import Game from './components/Game.jsx';
+import '../styles/_cssReset.css';
+import '../styles/_cssUtil.css';
+import '../styles/App.css';
+import Game from './Game.jsx';
 
 function App() {
   return (
